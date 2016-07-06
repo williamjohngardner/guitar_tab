321a2425cab7cd822aacc2f0dd7cfe56cbca7c91
williamjohngardner <bill@williamjohngardner.com>
2016-07-06 14:47:50 -0400

    tab list displays on list view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 
-def tab_list_view(request):
-    return render(request, "index.html", {})
+
+
+def tab_search_view(request):
+    artist = request.GET.get("artist")
+    letter = artist[0]
+    url = "http://www.bigbasstabs.com/{}/{}_bass_tabs.html".format(letter, artist)
+    content = requests.get(url).text
+    souper = BeautifulSoup(content, "html.parser")
+    results = souper.find_all(class_="song-list")
+
+    return render(request, "index.html", {"results": results})
--- guitar_tab/urls.py
+++ guitar_tab/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from app.views import tab_list_view
+from app.views import tab_search_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', tab_list_view, name="tab_list_view")
+    url(r'^$', tab_search_view, name="tab_list_view"),
+    url(r'^(?P<url>.+)', tab_detail_view, name="tab_detail_view")
 ]
