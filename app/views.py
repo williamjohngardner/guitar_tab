425fd5999d3c8a3108a133b912bee4b771f4141a
williamjohngardner <bill@williamjohngardner.com>
2016-07-06 16:56:53 -0400

    I can now search by Songs

--- app/views.py
+++ app/views.py
@@ -13,11 +13,22 @@ def tab_detail_view(request, url):
 
 
 def tab_search_view(request):
-    artist = request.GET.get("artist").lower().replace(" ", "_")
-    letter = artist[0]
-    url = "http://www.bigbasstabs.com/{}/{}_bass_tabs.html".format(letter, artist)
-    content = requests.get(url).text
-    souper = BeautifulSoup(content, "html.parser")
-    results = souper.find_all(class_="song-list")
+    if request.GET.get("artist"):
+        artist = request.GET.get("artist").lower().replace(" ", "_")
+        letter = artist[0]
+        url = "http://www.bigbasstabs.com/{}/{}_bass_tabs.html".format(letter, artist)
+        content = requests.get(url).text
+        souper = BeautifulSoup(content, "html.parser")
+        #print(souper.find(id="content").find())
+        results = str(souper.find(class_="song-list"))
 
-    return render(request, "index.html", {"results": results, "artist": artist})
+        return render(request, "index.html", {"results": results, "artist": artist})
+
+    elif request.GET.get("song"):
+        song = request.GET.get("song").lower().replace(" ", "_")
+        url = "http://www.bigbasstabs.com/search.html?search={}&type=songs".format(song)
+        content = requests.get(url).text
+        souper = BeautifulSoup(content, "html.parser")
+        results = str(souper.find(class_="song-list"))
+
+        return render(request, "index.html", {"results": results, "song": song})
